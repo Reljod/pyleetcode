dbe8299ae46d3e110e81bfeb8e22aa0b815a38a5
Reljod Oreta <oretareljod@gmail.com>
2024-06-04 00:58:42 +0800

    Add a draft binary search problem with question and solution

---
+++ binary_search/001_problem.py
@@ -0,0 +1,53 @@
+"""
+Disclaimer: This is just a sample problem I personally drafted.
+The question might not be as descriptive or complete as what a regular
+coding problem would have.
+
+
+Problem:
+
+Find the index of given number (target) on a sorted num array (sorted_arr)
+using binary search algorithm.
+
+target = any num
+sorted_arr = sorted non-repeating num array
+
+"""
+
+import sys
+
+def main(*args):
+    sorted_arr = [-1, 0, 3, 5, 9, 11]
+    target = 9
+
+    target_ind = find_target_ind(target, sorted_arr)
+
+    print(f"Target {target} is {'Missing' if target_ind == -1 else f'in {target_ind}' }")
+
+def find_target_ind(target: int, arr: list[int]) -> int:
+    l_ind = 0
+    r_ind = len(arr) - 1
+
+    while (l_ind <= r_ind):
+        m_ind = (l_ind + r_ind) // 2
+
+        if arr[m_ind] > target:
+            r_ind = m_ind
+        elif arr[m_ind] < target:
+            l_ind = m_ind + 1
+        else:
+            # Means arr[m_ind] == target
+            return m_ind
+
+    return -1
+
+def test_find_target_ind() -> None:
+    sorted_arr = [21, 32, 33, 59, 101, 121, 1903, 45001, 100000]
+
+    input_expected_map = [(x, sorted_arr.index(x)) for x in sorted_arr]
+
+    for (target, expected) in input_expected_map:
+        assert find_target_ind(target, sorted_arr) == expected
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
