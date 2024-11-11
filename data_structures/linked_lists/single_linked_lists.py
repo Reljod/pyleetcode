34036a85b0a7a2a0193146c1d482bacc68e1080b
Reljod Oreta <oretareljod@gmail.com>
2024-11-11 19:09:33 +0800

    Implement basic operations in linked lists

--- data_structures/linked_lists/single_linked_lists.py
+++ data_structures/linked_lists/single_linked_lists.py
@@ -1,4 +1,4 @@
-from typing import Any, Self
+from typing import Any, Callable, Self
 
 
 class Node:
@@ -28,19 +28,61 @@ class SingleLinkedListBuilder:
 
         return self
 
+    def insert(self, head: Node | None, node: Node, at: Node) -> None:
+        curr = head
+
+        while curr is not None:
+            if curr == at:
+                temp = curr.next
+                node.next = temp
+                curr.next = node
+
+            curr = curr.next
+
+    def remove(self, head: Node | None, node: Node) -> None:
+        curr = head
+
+        while curr is not None:
+            if curr.next == node:
+                curr.next = node.next
+
+            curr = curr.next
+
+    def filter(self, head: Node | None, predicate: Callable[[Node], bool]) -> Node | None:
+        curr = head
+        filtered = None
+
+        while curr is not None:
+            if predicate(curr):
+                if filtered is None:
+                    filtered = curr
+                else:
+                    filtered.next = curr
+                    filtered = filtered.next
+
+            curr = curr.next
+
+        if filtered is not None:
+            filtered.next = None
+
+        return filtered
+
     def build(self):
         return self.head
 
     def display(self, head: Node | None) -> None:
-        while head is not None:
-            print(head.val)
-            head = head.next
+        curr: Node | None = head
+
+        while curr is not None:
+            print(curr.val)
+            curr = curr.next
 
 
 def main():
     a = Node(10)
     b = Node(20)
     c = Node(30)
+    d = Node(40)
 
     sb = SingleLinkedListBuilder()
     head = sb.add(a)\
@@ -48,8 +90,20 @@ def main():
              .add(c)\
              .build()
 
+    print("Display linked list after adding")
     sb.display(head)
 
+    print("Display linked list after insertion")
+    sb.insert(head, d, b)
+    sb.display(head)
+
+    print("Display linked list after deletion")
+    sb.remove(head, d)
+    sb.display(head)
+
+    print("Display linked list after filter")
+    result = sb.filter(head, lambda x: x.val >= 20)
+    sb.display(result)
 
 if __name__ == "__main__":
     main()
